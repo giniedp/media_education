9618668f73347125a4152bda86f44f115eb767d0
Hauke Schade <cnlpete@cnlpete.de>
2011-05-30 13:01:22 +0200

    practice part of vocabulary mdoule done, except styling

--- javascripts/application/module.vocabulary.js
+++ javascripts/application/module.vocabulary.js
@@ -8,6 +8,8 @@
     whereAmIData   : {},
     navigationData : {},
     wisdomLevel    : 2,
+    vocData       : [],
+    vocHandle     : 0,
     statsTemplate  : _.template("<div title='Du hast bisher <%= statistics.wrongAnswers | 0 %> falsche und <%= statistics.correctAnswers | 0 %> richtige Antworten gegeben.'> <span style='color:red;'><%= statistics.wrongAnswers | 0 %></span> / <span style='color:green;'><%= statistics.correctAnswers | 0 %></span> </div>"),
     evaluateHash  : function(hash){
       App.Controller.pickModule(this.name);
@@ -27,7 +29,7 @@
       //get array of vocabulary
       //ordered: 
       //wisdomLevel determines, how often a vocable is max checked correctly, put this to disable
-      getVocabulary : function(n, learn){
+      getVocabulary : function(learn, callback, n){
         if(n === undefined || n < 0) n = 5; //how many?
         var ordered = 0;
         var wisdom = App.Modules.vocabulary.wisdomLevel;
@@ -37,39 +39,109 @@
           wisdom = -1;  //wisdomLevel?
         }
         
+        callback = (callback || App.Modules.vocabulary.Controller.appendVocData);
+        if (typeof(callback) === "function"){
+            callback.call(this, { vocabulary : [{
+            origin  : "Hello",
+            lang    : "de",
+            translations : ["Hallo"],
+            correctGuesses : 2
+          },{
+            origin  : "die Banane",
+            lang    : "en",
+            translations : ["banana"],
+            correctGuesses : 1
+          },{
+            origin  : "der Apfel",
+            lang    : "en",
+            translations : ["apple"],
+            correctGuesses : 0
+          },{
+            origin  : "das Haus",
+            lang    : "en",
+            translations : ["house"],
+            correctGuesses : 0
+          },{
+            origin  : "die Wohnung",
+            lang    : "en",
+            translations : ["apartment", "accommodation", "flat", "residence"],
+            correctGuesses : 0
+          }]});
+        }
+        
         //TODO server logic + userid ...
         //temp data for now...
-        return { vocabulary : [{
-          origin  : "Hello",
-          lang    : "de",
-          translations : ["Hallo"],
-          correctGuesses : 2
-        },{
-          origin  : "die Banane",
-          lang    : "en",
-          translations : ["banana"],
-          correctGuesses : 1
-        },{
-          origin  : "der Apfel",
-          lang    : "en",
-          translations : ["apple"],
-          correctGuesses : 0
-        },{
-          origin  : "das Haus",
-          lang    : "en",
-          translations : ["house"],
-          correctGuesses : 0
-        },{
-          origin  : "die Wohnung",
-          lang    : "en",
-          translations : ["apartment", "accommodation", "flat", "residence"],
-          correctGuesses : 0
-        }]};
+        return ;
       },
-      initializeExercises : function(learn){
+      appendVocData : function(data){
+        App.Modules.vocabulary.vocData = App.Modules.vocabulary.vocData.concat(data.vocabulary);
+      },
+      initializeExercises : function(data){
+        var voc = App.Modules.vocabulary;
         //load and show exercises... -> getVocabulary()
-        //add handler to reload vocs...
-        //add handler to send vocs back to server, when used.
+        if(data) {
+          App.Modules.vocabulary.Controller.appendVocData(data);
+        }
+        
+        //prefetch vocabulary
+        if(App.Modules.vocabulary.vocData.length <= App.Modules.vocabulary.vocHandle) {
+          voc.Controller.getVocabulary(false, App.Modules.vocabulary.Controller.initializeExercises);
+          return;
+        }
+        
+        var content = App.View.content.find("#exercise");
+        //fade and disable old exercises...
+        var exercises = content.find('.exercise');
+        for(var i = 0; i < exercises.length; i++) {
+          var obj = $(exercises[i]);
+        
+          var inp = obj.find("input.result")[0];
+          inp.disabled = true;
+          var opac = (obj.css("opacity") || 1) - 0.2;
+          if(opac > 0)
+            obj.fadeTo("slow", opac);
+          if(opac == 0) {
+            obj.fadeTo("slow", 0, function() {
+              $(this).remove();
+            })
+          }
+        }
+
+        var obj = App.Modules.vocabulary.vocData[App.Modules.vocabulary.vocHandle];
+        obj.id = App.Modules.vocabulary.vocHandle;
+        App.Modules.vocabulary.vocHandle += 1;
+        var temp = _.template('<div class="exercise" id="<%= id %>"><%= origin %><input class="result" type="text" autocomplete="off" onchange="App.Modules.vocabulary.Controller.validateExercise($(this).parent());"></input><span class="validation"></span></div>',obj);
+        $(temp).hide().prependTo(content).fadeIn(500, function() {
+          $('#exercise input.result')[0].focus();
+        });
+      },
+      validateExercise : function(container){
+        var id = container.attr("id");
+        var input = container.find("input.result");
+        var userResult = input.val();
+
+        var result = false;
+        for(var i=0; i < App.Modules.vocabulary.vocData[id].translations.length; i++) {
+          if(App.Modules.vocabulary.vocData[id].translations[i].toLowerCase() === userResult.toLowerCase()) {
+            result = true;
+            break;
+          }
+        }
+        
+        var stats = App.currentUser.moduleStatistics(App.currentModule);
+        //TODO send result to server
+        if (result){
+          input.animate({ "background-color" : "green" });
+          stats.correctAnswers = (stats.correctAnswers | 0) + 1;
+        } else {
+          input.animate({ "background-color" : "red" });
+          stats.wrongAnswers = (stats.wrongAnswers | 0) + 1;
+        }
+        
+        var waiContent = App.View.templates.whereami(App.Modules.vocabulary.whereAmIData);
+        App.Controller.swapContent(App.View.info, waiContent, 0);
+        
+        App.Modules.vocabulary.Controller.initializeExercises();
       },
       showLearnPage : function(name){
         name = name || "index";
@@ -94,7 +166,7 @@
         
         $.get("modules/vocabulary/learn.html", function(data){
             App.Controller.swapContent(App.View.content, data, function(){
-              App.Modules.vocabulary.Controller.initializeExercises(true);
+              //App.Modules.vocabulary.Controller.initializeExercises(true);
             });
             App.Controller.swapContent(App.View.sidebar, navContent, 0);
             App.Controller.swapContent(App.View.info, waiContent, 0);
@@ -123,7 +195,7 @@
 
         $.get("modules/vocabulary/practice.html", function(data){
             App.Controller.swapContent(App.View.content, data, function(){
-              App.Modules.vocabulary.Controller.initializeExercises(false);
+              App.Modules.vocabulary.Controller.initializeExercises();
             });
             App.Controller.swapContent(App.View.sidebar, navContent, 0); 
             App.Controller.swapContent(App.View.info, waiContent, 0);         
